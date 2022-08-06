defd60b25e3e126c06b123d37efbba068005b04e
Aurelian Constantin <aurelianconstantin2020@gmail.com>
2022-08-06 16:45:55 +0300

    extract teamToRow function

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function teamToRow(team) {
+  return `
+  <tr>
+  <td>${team.promotion}</td>
+  <td>${team.members}</td>
+  <td>${team.project}</td>
+  <td><a href="${team.url}">${team.url}</a></td>
+  <td>x e</td>
+  </tr>`;
+}
+
 function loadTeams() {
   fetch("data/teams.json")
     .then(function (r) {
@@ -9,22 +20,10 @@ function loadTeams() {
 }
 
 function teamsToTable(teams) {
-  // var tbl = document.querySelector("#teams tbody");
-  // tbl.innerHTML = "";
-  // for (var i = 0; i < teams.length; i++) {
-  //   tbl.innerHTML += `<tr><td>${teams[i].promotion}</td><td>${teams[i].members}</td><td>${teams[i].project}</td><td><a href="${teams[i].url}">${teams[i].url}</a></td></tr>`;
-
   var teamsHTML = "";
   // formateaza din json in html
   teams.forEach(function (team) {
-    teamsHTML += `
-            <tr>
-            <td>${team.promotion}</td>
-            <td>${team.members}</td>
-            <td>${team.project}</td>
-            <td><a href="${team.url}">${team.url}</a></td>
-            <td>x e</td>
-            </tr>`;
+    teamsHTML += teamToRow(team);
   });
   // afisare in table body
   document.querySelector("table tbody").innerHTML = teamsHTML;
